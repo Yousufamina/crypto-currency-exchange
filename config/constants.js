f7df19e15687e6a19864e2962025716a2d1fcd21
aamna <aminayousuf_13@yahoo.com>
2022-03-03 09:20:18 +0500

    added

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,7 @@
 const CONSTANT = {
     "mongoURI": "mongodb+srv://apollo355:apollo355@cluster0.utdjz.mongodb.net/crypto_currency?retryWrites=true&w=majority",
     "domainUrl": "http://93.188.166.110:4000",
-    "adminEmail1": "apollo355@yahoo.com",
+    "adminEmail": "apollo355@yahoo.com",
     "jwtSecret": "projectCryptoCurrencyExchange^#@#$"
 };
 
